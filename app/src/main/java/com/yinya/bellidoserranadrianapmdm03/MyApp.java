69a704bc131bdaa8c4416373a9e8207302b64982
ABSerran <adri.b.serran@gmail.com>
2025-01-23 18:46:32 +0100

    Add language settings

---
+++ app/src/main/java/com/yinya/bellidoserranadrianapmdm03/MyApp.java
@@ -0,0 +1,48 @@
+package com.yinya.bellidoserranadrianapmdm03;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.util.Log;
+
+import java.util.Locale;
+
+public class MyApp extends Application {
+    protected static final String PREFS_NAME = "AppPreferences";
+    protected static final String KEY_LANGUAGE = "language";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        String savedLanguage = getSavedLanguage();
+        Log.d("language", savedLanguage);
+        applyLanguage(savedLanguage);
+    }
+
+    public void applyLanguage(String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Configuration config = new Configuration();
+        config.setLocale(locale);
+
+        Context context = getBaseContext().createConfigurationContext(config);
+        getBaseContext().getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+    }
+
+    private String getSavedLanguage() {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return preferences.getString(KEY_LANGUAGE, "en");
+    }
+
+    public void saveLanguage(String language) {
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString(KEY_LANGUAGE, language);
+        editor.apply();
+    }
+
+
+}
--- app/src/main/java/com/yinya/bellidoserranadrianapmdm03/ui/SettingsFragment.java
+++ app/src/main/java/com/yinya/bellidoserranadrianapmdm03/ui/SettingsFragment.java
@@ -3,15 +3,25 @@ package com.yinya.bellidoserranadrianapmdm03.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.firebase.ui.auth.AuthUI;
+import com.yinya.bellidoserranadrianapmdm03.MyApp;
+import com.yinya.bellidoserranadrianapmdm03.R;
 import com.yinya.bellidoserranadrianapmdm03.databinding.FragmentSettingsBinding;
 
 public class SettingsFragment extends Fragment {
@@ -30,12 +40,26 @@ public class SettingsFragment extends Fragment {
         binding = FragmentSettingsBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         startLogoutListening();
+        startLanguageMenuListening();
+
+        MyApp app = (MyApp) requireActivity().getApplication();
         return view;
     }
 
     public void startLogoutListening() {
         TextView logoutBtn = binding.tvLogoutBtn;
         logoutBtn.setOnClickListener(this::onLogoutClick);
+    }
+
+    public void startLanguageMenuListening() {
+        TextView languageBtn = binding.tvLanguage;
+        languageBtn.setOnClickListener(this::onLanguageClick);
+    }
+
+    private void onLanguageClick(View v) {
+        PopupMenu popupMenu = inflateLanguageMenu(v);
+        popupMenu.setOnMenuItemClickListener(menuItem -> onContextItemSelected(menuItem));
+        popupMenu.show();
     }
 
     private void onLogoutClick(View v) {
@@ -51,6 +75,36 @@ public class SettingsFragment extends Fragment {
         Intent i = new Intent(requireContext(), LoginActivity.class);
         startActivity(i);
         getActivity().finish();
+    }
+
+    public PopupMenu inflateLanguageMenu(View v) {
+        PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+        MenuInflater inflater = requireActivity().getMenuInflater();
+        inflater.inflate(R.menu.language_menu, popupMenu.getMenu());
+        return popupMenu;
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        MyApp app = (MyApp) requireActivity().getApplication();
+        String selectedLanguage;
+
+        if (item.getItemId() == R.id.menu_selectable_english) {
+            selectedLanguage = "en";
+        } else if (item.getItemId() == R.id.menu_selectable_spanish) {
+            selectedLanguage = "es";
+        } else {
+            Log.e("Context menu", "Unknown item");
+            return false;
+        }
+
+        app.saveLanguage(selectedLanguage);
+        app.applyLanguage(selectedLanguage);
+        Log.d("language menu", selectedLanguage);
+
+        // TODO Restart activity
+
+        return true;
     }
 
     @Override
